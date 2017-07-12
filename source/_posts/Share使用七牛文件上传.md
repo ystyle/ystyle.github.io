92cc2f17a3de92109bee6694d2dbac5d93d028ba
ystyle <lxy5266@live.com>
2017-07-12 15:52:27 +0800

    添加博客与更新主题并把评论插件更换为gitment

---
+++ source/_posts/Share使用七牛文件上传.md
@@ -0,0 +1,65 @@
+---
+title: ShareX使用七牛文件上传
+date: 2017-07-05 14:06:26
+permalink: share-the-use-of-qiniu-file-upload
+tags:
+  - ShareX
+  - 软件
+categories:
+  - 软件
+---
+- 配置界面图
+
+![](http://osloqpukl.bkt.clouddn.com/201717051546-t.png)
+
+
+### 配置文件
+- 注意
+- token字段得自己在七牛的其它工具生成， 本文后面提供一个我制作的小工具
+- 上传的文件名格式为 `201717051546-t.png`,  `-`后面的为随机数字或字母。
+- 文件保存为 `qiniu.sxcu` 然后在上图所示界面导入
+- `RequestURL` 可能因为每个对象存储的区域不一样， 华东，华北，华南，北美 的地址分别为 `http://up-z0.qiniu.com/` `http://up-z1.qiniu.com/` `http://up-z2.qiniu.com/` `http://up-na0.qiniu.com/`
+- URL字段在七牛存储空间的内容管理页面的`外链默认域名` 下拉菜单能找到
+
+```json
+{
+  "Name": "qiniu",
+  "DestinationType": "ImageUploader, FileUploader",
+  "RequestType": "POST",
+  "RequestURL": "http://up-z2.qiniu.com/",
+  "FileFormName": "file",
+  "Arguments": {
+    "token": "这个填写自己的",
+    "key": "%y%yy%d%h%mi-%ra.png",
+    "file": "$input$"
+  },
+  "ResponseType": "Text",
+  "URL": "http://osloqpukl.bkt.clouddn.com/$json:key$"
+}
+```
+
+### qiniu token 获取工具
+本工具支持 windows, linux, osx , 使用方法如下
+
+```shell
+$ qiniutoken -h
+Usage of qiniutoken:
+  -ak string
+        ACCESS_KEY
+  -bk string
+        bucket name
+  -ex uint
+        Expires (default 15552000)
+  -sk string
+        SECRET_KEY
+
+# windows使用示例: 按着shift 右键文件夹空白打开命令行，输入以下命令， 把ak sk bk换成自己的
+qiniutoken.exe -ak=lPNn5sBYjqUhn_DhMOkHzoznYM3KwUt2sE1W21F1 -sk=E45Ox_RlUdrz0YOtasiuerLtKZxALiX9-7NduzHT -bk=images
+```
+下载地址：链接：http://pan.baidu.com/s/1pKIo0R5 密码：wfew
+
+参数说明：
+- ak ACCESS_KEY 在七牛个人中心密钥管理界面获取
+- sk SECRET_KEY 在七牛个人中心密钥管理界面获取
+- bk bucket name 存储空间的免名称
+- ex Expires time , token过期时间 默认 `3600 * 24 * 180 `(180天)
--- themes/indigo/scripts/plugins.js
+++ themes/indigo/scripts/plugins.js
@@ -1,5 +1,17 @@
-const version = require('../package.json').version
+const { version, name } = require('../package.json')
+
 hexo.extend.helper.register('theme_version', () => version)
+
+const source = (path, cache, ext) => {
+    if (cache) {
+        const minFile = `${path}${ext === '.js' ? '.min' : ''}${ext}`
+        return hexo.theme.config.cdn ? `//unpkg.com/${name}@latest${minFile}` : `${minFile}?v=${version}`
+    } else {
+        return path + ext
+    }
+}
+hexo.extend.helper.register('theme_js', (path, cache) => source(path, cache, '.js'))
+hexo.extend.helper.register('theme_css', (path, cache) => source(path, cache, '.css'))
 
 function renderImage(src, alt = '', title = '') {
     return `<figure class="image-bubble">
@@ -11,26 +23,27 @@ function renderImage(src, alt = '', title = '') {
             </figure>`
 }
 
-hexo.extend.tag.register('image', ([src, alt, title]) => {
-    return renderImage(src, alt, title)
+hexo.extend.tag.register('image', ([src, alt = '', title = '']) => {
+    return hexo.theme.config.lightbox ? renderImage(src, alt, title) : `<img src="${src}" alt="${alt}" title="${title}">`
 })
 
 hexo.extend.filter.register('before_post_render', data => {
+    if (hexo.theme.config.lightbox) {
+        // 包含图片的代码块 <escape>[\s\S]*\!\[(.*)\]\((.+)\)[\s\S]*<\/escape>
+        // 行内图片 [^`]\s*\!\[(.*)\]\((.+)\)([^`]|$)
+        data.content = data.content.replace(/<escape>.*\!\[(.*)\]\((.+)\).*<\/escape>|([^`]\s*|^)\!\[(.*)\]\((.+)\)([^`]|$)/gm, match => {
 
-    // 包含图片的代码块 <escape>[\s\S]*\!\[(.*)\]\((.+)\)[\s\S]*<\/escape>
-    // 行内图片 [^`]\s*\!\[(.*)\]\((.+)\)([^`]|$)
-    data.content = data.content.replace(/<escape>.*\!\[(.*)\]\((.+)\).*<\/escape>|([^`]\s*|^)\!\[(.*)\]\((.+)\)([^`]|$)/gm, match => {
+            // 忽略代码块中的图片
+            if (/<escape>[\s\S]*<\/escape>|.?\s{3,}/.test(match)) {
+                return match
+            }
 
-        // 忽略代码块中的图片
-        if (/<escape>[\s\S]*<\/escape>|.?\s{3,}/.test(match)) {
-            return match
-        }
-
-        return match.replace(/\!\[(.*)\]\((.+)\)/, (img, alt, src) => {
-            const attrs = src.split(' ')
-            const title = (attrs[1] && attrs[1].replace(/\"|\'/g, '')) || ''
-            return `{% image ${attrs[0]} '${alt}' '${title}' %}`
+            return match.replace(/\!\[(.*)\]\((.+)\)/, (img, alt, src) => {
+                const attrs = src.split(' ')
+                const title = (attrs[1] && attrs[1].replace(/\"|\'/g, '')) || ''
+                return `{% image ${attrs[0]} '${alt}' '${title}' %}`
+            })
         })
-    })
+    }
     return data
 })
